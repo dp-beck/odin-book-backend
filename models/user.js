29fb6b1501e9b2b1b5fa1e4449f9032e5117ee69
Daniel Beck <beckdp01@gmail.com>
2024-05-16 16:44:04 -0400

    create user model

--- models/user.js
+++ models/user.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-// TO DO: DEFINE Schema Values; Research how to handle user profile photos
-
 const UserSchema = new Schema({
-    profilePhoto,
-    userName,
-    password,
-    firstName,
-    lastName,
-    email,
-    aboutMe,
-    posts,
-    friends,
+    profilePhoto: Buffer, // Storing photos as binary date directly in the MongoDB document
+    userName: { type: String, required: true, maxLength: 100 },
+    password: { type: String, required: true, maxLength: 100, minLength: 6 },
+    firstName: { type: String, required: true, maxLength: 100 },
+    lastName: { type: String, required: true, maxLength: 100 },
+    email: { type: String, required: true, maxLength: 100 },
+    aboutMe: { type: String, required: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
+
+// Virtual for author's URL
+UserSchema.virtual("url").get(function () {
+    // We don't use an arrow function as we'll need the this object
+    return `/users/${this._id}`;
+  });
+
+module.exports = mongoose.model("User", UserSchema);
