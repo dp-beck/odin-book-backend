64d8c2028ebe1a90cfe5937ecc42f889c13b5ac1
Daniel Beck <beckdp01@gmail.com>
2024-05-20 13:01:39 -0400

    write dummy controllers and routes for user endpoints

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+
+// Return a list of all Users
+exports.user_list = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: USER LIST");
+});
+
+// Return details for a specific user
+exports.user_detail = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: User Details: ${req.params.id}`);
+});
+
+// Create a new user
+exports.user_signup = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: User Signup`);
+});
+
+// Delete a user
+exports.user_delete = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: User Delete: ${req.params.id}`);
+});
+
+// Update a user's Details
+exports.user_update = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: User Details Update: ${req.params.id}`);
+});
+
+// Send a Friend Request
+exports.user_request_friend = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Send a Friend Request from: ${req.params.id}`);
+});
+
+// Accept a Friend Request
+exports.user_accept_friend_request = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Accept a Friend Request for: ${req.params.id}`);
+});
+
+// Log In a User
+exports.user_login = asyncHandler(async (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Log In User: ${req.params.id}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/userController');
 
 /* GET home page. DUMMY CONTENT CURRENTLY TO MAKE SURE TESTING WORKS*/
 router.get('/', function(req, res, next) {
   res.json({ name: "frodo"});
 });
+
+/* AUTHENTICATION ROUTE */
+
+/* USERS ROUTES */
+
+// GET list of users
+router.get('/users', userController.user_list);
+
+// GET individual user details
+router.get('/users/:id', userController.user_detail);
+
+// POST create new user
+router.post('users/signup', userController.user_signup);
+
+// DELETE user
+router.delete('users/delete', userController.user_delete);
+
+// POST update new user details
+router.post('users/:id/update', userController.user_update);
+
+// POST update active friend request (send friend request)
+router.post('users/:id/request_friend', userController.user_request_friend)
+
+// POST update users' friends list (accept friend request)
+router.post('users/:id/accept_friend_request', userController.user_accept_friend_request);
+
+// POST login user
+router.post('users/:id/login', userController.user_login);
+
+/* POSTS ROUTES */
+
+// GET post text
+// GET comments for a post
+// POST create new post
+// POST update a post
+// DELETE post
+
+/* COMMENTS ROUTES */
+
+// GET comment text
+// POST create new comment
+// POST update a comment
+// DELETE a comment
 
 module.exports = router;
