94ec8313e0a9cc2b97b48fe176d8f74f34915fa7
daniel-beck <beckdp01@gmail.com>
2024-07-18 19:11:12 -0400

    add cors middleware to enable cross origin fetch requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
